6a3a4616f70fffe1015715054cbf563582b054fb
muhammadniko <mn.muhammadniko@gmail.com>
2026-05-04 13:58:34 +0700

    Perbaikan Autorun Script

--- WhatsApp Connect.bat
+++ WhatsApp Connect.bat
@@ -1,6 +1,21 @@
 @echo off
+
 cd /d C:\xampp\htdocs\klunting-bot
 
+color 0e
+
+title WhatsApp Connect - Klunting App
+
+echo  _  ___     _    _ _   _ _______ _____ _   _  _____ 
+echo ^| ^|/ / ^|   ^| ^|  ^| ^| \ ^| ^|__   __^|_   _^| \ ^| ^|/ ____^|
+echo ^| ' /^| ^|   ^| ^|  ^| ^|  \^| ^|  ^| ^|    ^| ^| ^|  \^| ^| ^|  __ 
+echo ^|  ^< ^| ^|   ^| ^|  ^| ^| . ` ^|  ^| ^|    ^| ^| ^| . ` ^| ^| ^|_ ^|
+echo ^| . \^| ^|___^| ^|__^| ^| ^|\  ^|  ^| ^|   _^| ^|_^| ^|\  ^| ^|__^| ^|
+echo ^|_^|\_\______\____/^|_^| \_^|  ^|_^|  ^|_____^|_^| \_^|\_____^|
+echo.
+echo App Versi 1.0 - Build by Niko
+echo.                                             
+echo.
 echo [%date% %time%] Mulai eksekusi script...
 
 echo [%date% %time%] Menghapus folder auth...
@@ -12,7 +27,10 @@ if exist auth (
  )
 
 echo [%date% %time%] Menjalankan Klunting Bot...
+echo.
+echo.
 node index.js
 
 echo [%date% %time%] Proses Klunting Bot selesai.
+echo.
 pause
